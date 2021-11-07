0a533c31a4c418f378469ef20fe3567606bdd8b2
Jay <jwpyo98@gmail.com>
2021-11-07 11:05:29 +0900

    - fix "임시 포인트 지우기" - fix "chart width 키움"

--- application-directory/soundmind/mobile/src/main/java/com/jwpyo/soundmind/view/history/StressLineChart.kt
+++ application-directory/soundmind/mobile/src/main/java/com/jwpyo/soundmind/view/history/StressLineChart.kt
@@ -104,18 +104,10 @@ constructor(
     }
 
     companion object {
-        const val WIDTH_PER_HOUR = 300
+        const val WIDTH_PER_HOUR = 500
 
         val defaultInfo: List<Stress> = listOf(
-            Stress(LocalDateTime.now().toLocalDate().atTime(9, 50), 0.2f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(9, 55), 0.75f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(10, 0), 0.6f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(10, 5), 1.5f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(10, 10), 2.2f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(10, 15), 3.5f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(10, 20), 4.9f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(10, 25), 3.5f),
-            Stress(LocalDateTime.now().toLocalDate().atTime(10, 30), 2.2f),
+            Stress(LocalDateTime.now(),0.5f),
         )
     }
 }
