7f5400265afad4705f2bf7d778f2532fc55caf9d
jwpyo <jwpyo98@gmail.com>
2021-12-13 20:37:20 +0900

    targetSDK -> 31 Voice Entity Migration

--- application-directory/soundmind/mobile/src/main/java/com/jwpyo/soundmind/persistence/AppDatabase.kt
+++ application-directory/soundmind/mobile/src/main/java/com/jwpyo/soundmind/persistence/AppDatabase.kt
@@ -16,7 +16,7 @@ import com.jwpyo.soundmind.persistence.dao.VoiceDao
         Voice::class,
         Stress::class,
     ],
-    version = 1,
+    version = 2,
     exportSchema = true
 )
 @TypeConverters(
